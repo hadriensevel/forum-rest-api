dfa00e4000f943ca98643f48a84398d9460a24c1
hadriensevel <hadrien.sevel@outlook.com>
2023-03-30 11:59:53 +0200

    First implementation of the method to create a question

--- Controller/Api/QuestionController.php
+++ Controller/Api/QuestionController.php
@@ -45,6 +45,59 @@ class QuestionController extends BaseController
     }
 
     /**
+     * Create a new question
+     * @return void
+     */
+    public function create(): void
+    {
+        $strErrorDesc = $strErrorHeader = $responseData = '';
+
+        try {
+            // Check Content-Type
+            $contentType = isset($_SERVER["CONTENT_TYPE"]) ? trim($_SERVER["CONTENT_TYPE"]) : '';
+            if ($contentType !== 'application/json') {
+                $strErrorDesc = 'Content-Type must be application/json';
+                $strErrorHeader = 'HTTP/1.1 400 Bad Request';
+            } else {
+                // Read the raw POST data
+                $rawData = file_get_contents('php://input');
+
+                // Decode the JSON data into a PHP object or array
+                $postData = json_decode($rawData, true);
+
+                // TODO: Change the data here
+                if (isset($postData['title']) && isset($postData['content'])) {
+                    $questionModel = new QuestionModel();
+
+                    // Save the new question to the database
+                    $newQuestionId = $questionModel->addQuestion($postData['title'], $postData['content']);
+
+                    $responseData = array('id' => $newQuestionId);
+                } else {
+                    $strErrorDesc = 'Missing required fields';
+                    $strErrorHeader = 'HTTP/1.1 400 Bad Request';
+                }
+            }
+        } catch (Exception $e) {
+            $strErrorDesc = $e->getMessage();
+            $strErrorHeader = 'HTTP/1.1 500 Internal Server Error';
+        }
+
+        if (!$strErrorDesc) {
+            $this->sendOutput(
+                'HTTP/1.1 201 Created',
+                $responseData
+            );
+        } else {
+            $this->sendOutput(
+                $strErrorHeader,
+                array('error' => $strErrorDesc)
+            );
+        }
+    }
+
+
+    /**
      * Delete a question
      * @param int $id
      * @return void

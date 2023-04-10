ffb0e60efd9e23f924bbbd7f718306b9711e2d2e
hadriensevel <hadrien.sevel@outlook.com>
2023-04-10 15:44:58 +0200

    Remove unused PHPMailer SMTP

--- Mailer/Mailer.php
+++ Mailer/Mailer.php
@@ -6,7 +6,6 @@
  */
 
 use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
 class Mailer
@@ -32,7 +31,7 @@ class Mailer
         $this->mailer->SMTPAuth = true;                             //Enable SMTP authentication
         $this->mailer->Username = MAILER_USERNAME;                  //SMTP username
         $this->mailer->Password = MAILER_PASSWORD;                  //SMTP password
-        $this->mailer->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;    //Enable implicit TLS encryption
+        $this->mailer->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS; //Enable implicit TLS encryption
         $this->mailer->Port = MAILER_PORT;                          //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
         // Sender
